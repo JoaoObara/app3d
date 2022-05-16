b5549f6229a1d019a7ce16783f422f0b9bef268d
Joao Ricardo Obara <joao.ricardo.obara@gmail.com>
2022-05-16 08:22:32 -0400

    criando classe usuario

---
+++ lib/models/user.dart
@@ -0,0 +1,14 @@
+class User {
+  final String? id;
+  final String nome;
+  final String email;
+  // final String senha;
+  final bool admin;
+
+  const User(
+      {required this.id,
+      required this.nome,
+      required this.email,
+      // required this.senha,
+      required this.admin});
+}
